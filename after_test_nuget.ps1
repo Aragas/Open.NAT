40d7bb07663e91ca106186b69c65050000c97f02
aragas <personal@aragas.org>
2019-10-08 01:28:14 +0300

    no need for repo tag

--- after_test_nuget.ps1
+++ after_test_nuget.ps1
@@ -1,9 +1,6 @@
-if($env:CONFIGURATION -eq "Default" -and $env:APPVEYOR_REPO_TAG -eq "true" -and $isWindows)
-{
-	cd $env:APPVEYOR_BUILD_FOLDER
-	cd Open.Nat
-	nuget pack Open.Nat.nuspec -Version $env:APPVEYOR_BUILD_VERSION
-	
-	$nupkg = (Get-ChildItem Open.Nat*.nupkg)[0];
-	Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "Open.Nat.nupkg";
-}
+cd $env:APPVEYOR_BUILD_FOLDER
+cd Open.Nat
+nuget pack Open.Nat.nuspec -Version $env:APPVEYOR_BUILD_VERSION
+
+$nupkg = (Get-ChildItem Open.Nat*.nupkg)[0];
+Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "Open.Nat.nupkg";
