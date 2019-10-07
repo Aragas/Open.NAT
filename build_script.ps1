a6b59e1837eb5573c5e52fc9897b38e7cfee0827
aragas <personal@aragas.org>
2019-10-08 01:52:49 +0300

    Added platform property
    removed default nupkg

--- build_script.ps1
+++ build_script.ps1
@@ -1,4 +1,4 @@
 msbuild Open.Nat\Open.Nat.csproj /t:restore
 msbuild Open.Nat\Open.Nat.csproj /t:restore
-msbuild Open.Nat\Open.Nat.csproj /property:Configuration=Debug
-msbuild Open.Nat\Open.Nat.csproj /property:Configuration=Release
+msbuild Open.Nat\Open.Nat.csproj /property:Configuration=Debug /property:Platform=AnyCPU
+msbuild Open.Nat\Open.Nat.csproj /property:Configuration=Release /property:Platform=AnyCPU
